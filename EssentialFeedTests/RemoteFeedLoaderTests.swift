0fcca7c5fc22c276e64f2a1b2dae87113554b6c0
IslomiddinAgzamov <96278128+IslomiddinAgzamov@users.noreply.github.com>
2025-01-01 20:02:37 +0500

    remote feed loader does not request data upon creation

---
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by Islomiddin on 01/01/25.
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+    
+}
+
+class HTTPClient {
+    var requestedURL: URL?
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient()
+        _ = RemoteFeedLoader()
+        
+        XCTAssertNil(client.requestedURL)
+    }
+
+}
